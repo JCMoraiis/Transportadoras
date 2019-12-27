25316494c75f8897e6982f953da6674feadec841
Tarcisio Oliveira <tarcisio.oliveira@mv.com.br>
2019-12-27 17:09:44 -0300

    criar component de transportadora

---
+++ src/app/shipping-company/shipping-company.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { ShippingCompany } from '../models/shippingCompany.model';
+
+@Component({
+  selector: 'app-shipping-company',
+  templateUrl: './shipping-company.component.html',
+  styleUrls: ['./shipping-company.component.scss']
+})
+export class ShippingCompanyComponent implements OnInit {
+
+  @Input() shippingCompany: ShippingCompany;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
