35a642debbd492df1083b8c2c2d54bf9f5ab3bc3
Tarcísio Cipriano <creativecipriano@gmail.com>
2020-01-06 21:11:01 -0300

    alterar para nova forma de navegação

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,22 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, RoutesRecognized } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
 
-  isCreatingShippingCompany = false;
+  private buttonStatus: string;
+  private buttonStatusSub: Subscription;
 
   constructor(private router: Router) { }
 
   ngOnInit() {
+    this.buttonStatusSub = this.router.events.subscribe((data) => {
+      if (data instanceof RoutesRecognized) {
+        this.buttonStatus = data.state.root.firstChild.data['buttonStatus'];
+      }
+    });
   }
 
   onButton() {
-    this.isCreatingShippingCompany = !this.isCreatingShippingCompany;
+    if (this.buttonStatus === 'home') {
+      this.router.navigate(['/shippingCompanyForm']);
+    } else if (this.buttonStatus === 'form') {
+      this.router.navigate(['/home']);
+    }
+  }
+
+  ngOnDestroy() {
+    this.buttonStatusSub.unsubscribe();
   }
 
 }
